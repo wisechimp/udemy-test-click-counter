dd481c1d88d8a0cd4c1d7747875936fc5b70e1e8
shicks1080 <stuart.hicks@gmail.com>
2019-03-04 22:41:25 +0100

    Update App.js
    Added logic for increment and decrement buttons plus error message

--- src/App.js
+++ src/App.js
@@ -6,18 +6,53 @@ class App extends Component {
     super(props);
 
     this.state = {
-      counter: 0
+      counter: 0,
+      errorMessageIsHidden: true
     };
   }
 
+  increaseCounter() {
+    this.setState({ errorMessageIsHidden: true })
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  decreaseCounter () {
+    if (this.state.counter === 0) {
+      this.setState({ errorMessageIsHidden: false })
+    } else {
+      this.setState({ counter: this.state.counter - 1 })
+    }
+  }
+
   render() {
+
     return (
       <div data-test="component-app">
-        <h1 data-test="counter-display">The counter is currently</h1>
-        <button data-test="increment-button">Increment counter</button>
+        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+        {!this.state.errorMessageIsHidden && <ErrorMessage />}
+        <button
+          data-test="increment-button"
+          onClick={this.increaseCounter.bind(this)}
+          >
+          Increment counter
+        </button>
+        <button
+          data-test="decrement-button"
+          onClick={this.decreaseCounter.bind(this)}
+        >
+          Decrement counter
+        </button>
       </div>
     );
   }
 }
+
+const ErrorMessage = () => (
+  <div
+    data-test="error-message"
+    className="errorMessage">
+    You cannot decrease the counter below 0.
+  </div>
+)
 
 export default App;
